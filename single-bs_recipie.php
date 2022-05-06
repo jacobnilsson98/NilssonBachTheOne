1299e53e1ac7607f09ce61cfbc247ee1e7f3596f
jacob nilsson <jacobnilsson98@hotmail.com>
2022-05-06 18:28:44 +0200

    post styling

--- single-bs_recipie.php
+++ single-bs_recipie.php
@@ -16,30 +16,62 @@ if ( have_posts() ) :
     while ( have_posts() ) : the_post();
     
     if (get_the_post_thumbnail_url()){ 
-        ?><div class="d-flex container-fluid" style="height:50vh;background:url(<?php echo get_the_post_thumbnail_url(); ?>)  center / cover no-repeat;"></div>
+        ?>
+        
+        <div class="d-flex container-fluid" style="height:50vh;background:url(<?php echo get_the_post_thumbnail_url(); ?>)  center / cover no-repeat;"></div>
     <?php } else {
-        ?><div class="d-flex container-fluid" style="height:20vh;"></div>
+        
+        ?>
+        <div class="py-6 bg-light">
+    <div class="container text-center">
+        <h1 class="display-4"><?php the_title(); ?></h1>
+        
+  </div>
+</div><div class="d-flex container-fluid" style="height:20vh;"></div>
+        
+        
+
+
     <?php } ?>
     
     
 
     	</div><!-- #content -->
+
+        <div class="d-flex justify-content-center">
+            <div class="image-container">
 	<?php if( $featured_image ): ?>
-			<img class="featured_image" src="<?php echo $featured_image['url']; ?>" alt="<?php echo $featured_image['alt']; ?>" />
+			<img class="image" src="<?php echo $featured_image['url']; ?>" alt="<?php echo $featured_image['alt']; ?>" />
 		<?php endif; ?>
+            </div>
+        </div>
 
+        <div class="d-flex justify-content-center">
 		<?php if( $servings ): ?>
-		<pre><?php echo print_r($servings);?></pre>
+            <div class="servings"><h3>Servings</h3>
+		<div class="amount_servings"><pre><?php echo print_r($servings);?></pre></div>
+        </div>
 		<?php endif; ?>
+        </div>
 
+        <div class="d-flex justify-content-center">
 		<?php if( $ingredients ): ?>
-		<pre><?php echo print_r($ingredients);?></pre>
+            <div class="ingredients"><h3>Ingredients</h3>
+		<div class="amount_ingredients"></div><pre><?php echo print_r($ingredients);?></pre></div>
+        </div>
 		<?php endif; ?>
+        </div>
 
+        <div class="d-flex justify-content-center">
+            <div class="instructions-container"
 		<?php if( $instructions ): ?>
-		<?php echo print_r($instructions);?>
+            <div class="instuctions"><h3>Instructions</h3>
+		<div class="amount_instructions"></div><?php echo print_r($instructions);?></div>
+        </div>
 		<?php endif; ?>
-
+        </div>
+        </div>
+            
 
 
 <?php
