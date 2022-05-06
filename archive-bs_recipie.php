e2ae55df20a14211345743ba4b7030b14e5c98f3
jacob nilsson <jacobnilsson98@hotmail.com>
2022-05-06 14:43:05 +0200

    Fixed archive

--- archive-bs_recipie.php
+++ archive-bs_recipie.php
@@ -1,6 +1,7 @@
 <?php
+
 /**
- * Template Name: Template: archive
+ * Template Name: recipie archive
  *
  *
  * Template for displaying a page just with the header and footer area and a "naked" content area in between.
@@ -10,51 +11,68 @@
  */
 
 // Exit if accessed directly.
-defined( 'ABSPATH' ) || exit;
+defined('ABSPATH') || exit;
 
 get_header();
 ?>
 <div class="py-6 bg-light">
     <div class="container text-center">
-        <h1 class="display-4"><?php the_title(); ?></h1>
-        
-  </div>
+        <h1 class="display-4">Recepie</h1>
+
+    </div>
 </div>
 
 <div class="container">
     <div class="row">
-        <div class="col-md-9 py-5 order-2">
-            <?php 
-$image = get_field('featured_image');
-if( !empty( $image ) ): ?>
-<img style="height:500px; width:400px;" src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
-<?php endif; ?>
+       
 
-<h4><?php the_title(); ?></h4>
-<?php
-            if ( have_posts() ) : 
-                while ( have_posts() ) : the_post();
-                    the_content();
-                endwhile;
-            else :
-                _e( 'Sorry, no posts matched your criteria.', 'textdomain' );
-            endif;
-            ?>
+
+
+
+            <?php
+            $args = array(
+                'post_type' => 'bs_recipie',
+                'post_status' => 'publish',
+                'posts_per_page' => 8,
+                'order' => 'ASC',
+            );
+
+            $loop = new WP_Query($args);
+
+            while ($loop->have_posts()) : $loop->the_post();
+            ?><div class="col-md-4 py-5 order-2"> <?php
+                $image = get_field('featured_image');
+                if (!empty($image)) : ?>
+                    <img style="height:500px; width:400px;" src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
+
+                    <h4><?php the_title(); ?></h4><?php
+                                                the_excerpt();?>
+              </div>
+                <?php endif;
+
+               
+                                            endwhile;
+                                                ?>
         </div>
 
-        <div class="col-md-3 pt-5 order-1">
-            <?php 
-            get_sidebar();
+        <div class="col-md-1 pt-5 order-1">
+            <?php
+            // get_sidebar();
             ?>
         </div>
 
     </div>
 </div>
+<?php $categories = get_terms(['taxonomy' => 'bs_recipie_cat']); ?>
 
+<ul>
+    <?php foreach ($categories as $key => $category) { ?>
+        <li>
+            <a href="<?php echo esc_url(get_term_link($category)); ?>">
+                <?php echo esc_html($category->name); ?>
+            </a>
+        </li>
+    <?php } ?>
+</ul>
 
 <?php get_footer();
-
-
-
-
-            
